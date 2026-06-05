af54602da8d9658201cbeae491006540689657ae
Harsh Kumar <harsh_kumar@srmap.edu.in>
2026-06-05 18:29:25 +0530

    Fix Express 5 wildcard route bug

--- server/index.js
+++ server/index.js
@@ -191,7 +191,7 @@ io.on('connection', (socket) => {
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
 // Catch-all route to serve the React app
-app.get('*', (req, res) => {
+app.use((req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
